fc26569d6df5dd442dcb0062b764f8f060132205
okbp <yokookbio@gmail.com>
2020-10-02 15:34:28 +0900

    add load_all template

---
+++ bin/update/load_all_template.sh
@@ -0,0 +1,32 @@
+rake ontology:faldo:load 20200229
+rake ontology:taxonomy:load 20200229
+rake ontology:insdc:load 20200229
+rake ontology:obo_go:load 20200229
+rake ontology:obo_so:load 20200229
+rake ontology:meo:load 0.7
+rake ontology:meo_dag:load 0.9
+rake ontology:mpo:load 0.7
+rake ontology:gmo:load 0.11b
+rake ontology:mccv:load 0.99
+rake ontology:pdo:load 0.11
+rake ontology:pdo:load_lod 20160609
+rake ontology:csso:load 0.2
+rake ontology:gazetteer:load 20130906
+rake ontology:brc:load 20160609
+rake ontology:nbrc:load 20200229
+rake ontology:jcm:load 20200229
+rake ontology:gold:load 20150118
+
+rake linkage:load_meo_descendants 0.7
+rake linkage:load_mpo_descendants 0.7
+
+
+rake genomes:load 20200229
+rake refseq:load_refseq 98
+rake refseq:load_stats 98
+
+rake uniprot:load_tgup
+
+rake uniprot:load 202002
+rake uniprot:uniprot2stats
+rake uniprot:load_stats
